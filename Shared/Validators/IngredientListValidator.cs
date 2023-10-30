3baadd5a737918dc8d6fc1d06fd89ea851bd6b92
Emily Hatala <ehatala@bhg-inc.com>
2023-10-30 16:51:06 -0400

    some controls

---
+++ Shared/Validators/IngredientListValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Shared.Models;
+
+namespace Shared.Validators
+{
+    public class IngredientListValidator : AbstractValidator<IngredientList>
+    {
+        public IngredientListValidator()
+        {
+            RuleFor(x => x).NotEmpty();
+            RuleForEach(x => x.Ingredients).SetValidator(new IngredientValidator());
+        }
+
+        public Func<IngredientList, Task<IEnumerable<string>>> Validation => ValidateIngredientList;
+
+        private async Task<IEnumerable<string>> ValidateIngredientList(IngredientList ingredients)
+        {
+            ValidationResult result = await ValidateAsync(ingredients);
+
+            if (result.IsValid)
+            {
+                return Array.Empty<string>();
+            }
+            return result.Errors.Select(e => e.ErrorMessage);
+        }
+    }
+}
