9216787f749b4a7a08a34325b8f2b83a679fb9be
Emily Hatala <ehatala@bhg-inc.com>
2023-11-02 15:35:46 -0400

    Show validation messages.

--- Shared/Models/IngredientList.cs
+++ Shared/Models/IngredientList.cs
@@ -1,9 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Shared.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace Shared.Models
 {
     public class IngredientList
     {
+        [DuplicateIngredientValidator]
         public List<string> Ingredients { get; set; }
 
         [Required(AllowEmptyStrings = false)]
---
+++ Shared/Validation/DuplicateIngredientValidator.cs
@@ -0,0 +1,29 @@
+﻿using Shared.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Validation
+{
+    /// <summary>
+    /// Validates that <see cref="IngredientList.Ingredients"/> does not contain duplicate items.
+    /// </summary>
+    public class DuplicateIngredientValidator : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            List<string>? ingredients = value as List<string>;
+            IngredientList? ingredientList = validationContext?.ObjectInstance as IngredientList;
+            ArgumentNullException.ThrowIfNull(ingredients);
+            ArgumentNullException.ThrowIfNull(ingredientList);
+
+            if (!ingredients.Contains(ingredientList.SelectedIngredient))
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult("Ingredients cannot be duplicates.",
+                new List<string> { validationContext.MemberName });
+            }
+        }
+    }
+}
