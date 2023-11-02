144eccf52990c5d7ae5a740e0e39a649e325b9c8
Emily Hatala <ehatala@bhg-inc.com>
2023-11-02 14:24:28 -0400

    Got the validation working.

--- Shared/Models/IngredientList.cs
+++ Shared/Models/IngredientList.cs
@@ -1,9 +1,12 @@
-﻿namespace Shared.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Models
 {
     public class IngredientList
     {
         public List<string> Ingredients { get; set; }
 
+        [Required(AllowEmptyStrings = false)]
         public string SelectedIngredient { get; set; }
     }
 }
